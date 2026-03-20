34400a2200a955c184824efca8beca28cf32b696
kf0605 <zxcvb33800@gmail.com>
2026-03-20 11:34:26 +0900

    Add files via upload

---
+++ 0320/rnage.py
@@ -0,0 +1,6 @@
+sum = 0
+
+for i in range(1, 11):
+    sum += i    #sum = sum + i
+    print(i, end="")
+print("1부터 10까지의 합:", sum)
---
+++ 0320/스코어.py
@@ -0,0 +1,14 @@
+socre = int(input("Enter a score: "))
+
+match score//10:
+    case 1:
+        print("A")
+    case 2:
+        print("B")
+    case 3:
+        print("C")
+    case 4:
+        print("D")
+    case 5:
+        print("F")
+        
